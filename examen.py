30db14d9ddde70374729d77095abd2885190597d
examen <jg057440gmail.com>
2023-03-29 11:30:41 -0600

    texto

--- examen.py
+++ examen.py
@@ -6,3 +6,15 @@ num3= 100
 result= (num1+num2+num3)
 int=input ("tener el valor de la suma")
 print (result)
+
+num4= 350
+num5= 20
+result= (num4-num5)
+int=input ("tener el valor de la resta")
+print (result)
+
+num6= 50
+num7= 40
+result= (num6*num7)
+int=input ("tener el valor de la multiplicacion")
+print (result)
