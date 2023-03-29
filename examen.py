01ae2a7e3893725aec711489309733c83adf3815
examen <jg057440gmail.com>
2023-03-29 11:29:17 -0600

    texto

--- examen.py
+++ examen.py
@@ -2,14 +2,7 @@
 
 num1= 50
 num2= 20
-num3= 30
+num3= 100
 result= (num1+num2+num3)
 int=input ("tener el valor de la suma")
 print (result)
-
-num4= 30
-num5= 20
-result= (num4-num5)
-int=input ("tener el valor de la resta")
-print (result)
-
