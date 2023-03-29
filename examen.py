e3b99b8d40c2e238de42b359ff14eca68d888b7c
examen <jg057440gmail.com>
2023-03-29 11:13:41 -0600

    texto

---
+++ examen.py
@@ -0,0 +1,15 @@
+"pedir que sume o reste los numeros"
+
+num1= 50
+num2= 20
+num3= 30
+result= (num1+num2+num3)
+int=input ("tener el valor de la suma")
+print (result)
+
+num4= 30
+num5= 20
+result= (num4-num5)
+int=input ("tener el valor de la resta")
+print (result)
+
